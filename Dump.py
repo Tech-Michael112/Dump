39318c1c5575fb0de85da98b5f433ac2f32998b2
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2025-04-30 21:57:38 +0100

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -1,6 +1,6 @@
 import X,os
 try:
-  if os.path.exists('X.cpython-312.so').strip()
+  if os.path.exists('X.cpython-312.so') #.strip()
   os.system('git pull')
 except: 
   exit(' something went wrong ')
